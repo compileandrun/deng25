3722563e97ac7b42e0e4f5478e8c75ccf2315a79
Koray Can Canut <kccanut@gmail.com>
2025-04-16 09:14:18 +0000

    add external table

---
+++ Project/macros/stage_external_sources.sql
@@ -0,0 +1,39 @@
+abversion: 2
+
+sources:
+  - name: external_data
+    tables:
+      - name: binance_ext
+        description: This is an external table based on all files in the GCS bucket
+        external:
+            location: gs://elegant-bucket/aggtrade/binance_data
+        
+
+        columns:
+          - name: _dlt_id
+            data_type: varchar(255)
+            #description: "Application ID"
+          - name: _dlt_load_id
+            data_type: varchar(255)
+            #description: "Application ID"
+          - name: m
+            data_type: bool
+            #description: "Application ID"
+          - name: t
+            data_type: integer
+            #description: "Application ID"
+          - name: q
+            data_type: float
+            #description: "Application ID"
+          - name: l
+            data_type: integer
+            #description: "Application ID"
+          - name: p
+            data_type: float
+            #description: "Application ID"
+          - name: f
+            data_type: integer
+            #description: "Application ID"
+          - name: a
+            data_type: integer
+            #description: "Application ID"
